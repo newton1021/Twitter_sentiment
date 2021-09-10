e9c23285ae30853ad573cbc3980848ec51f53a6c
Geoffrey Flynn <gflynn@kmtel.com>
2021-09-09 23:10:13 -0500

    Database progress

---
+++ static/Query_Twitter.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+# this file is used to provide functions for querying Tweeter with a hashtag topic and adding the tweet to the "new data database"
+
--- twtDB_tables.sql
+++ twtDB_tables.sql
@@ -1,18 +1,28 @@
-CREATE TABLE "learning" (
-    "tweet" varchar,
-    "positivity_rank" int
-);
+-- Table: public.tweet_data
 
-DROP  Table tweet_data;
+-- DROP TABLE public.tweet_data;
 
-CREATE TABLE tweet_data (
-	"id" serial,
-	"name" varchar,
-	"date" varchar,
-	"retweet_count" int,
-    "tweet_text" varchar,
-    "tweet_cleaned" varchar,
-    "search_key" varchar,
-    "est_positivity" int,
-	primary key(id)
-);
+CREATE TABLE public.tweet_data
+(
+    id bigint,
+    name text COLLATE pg_catalog."default",
+    date text COLLATE pg_catalog."default",
+    retweet_count bigint,
+    tweet_text text COLLATE pg_catalog."default",
+    tweet_cleaned text COLLATE pg_catalog."default",
+    search_key text COLLATE pg_catalog."default",
+    est_positivity bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.tweet_data
+    OWNER to postgres;
+-- Index: ix_tweet_data_id
+
+-- DROP INDEX public.ix_tweet_data_id;
+
+CREATE INDEX ix_tweet_data_id
+    ON public.tweet_data USING btree
+    (id ASC NULLS LAST)
+    TABLESPACE pg_default;
